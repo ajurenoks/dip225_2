c612afb4d546c3e1009b47901fe064393e370c16
Aleksejs Jurenoks <40733171+ajurenoks@users.noreply.github.com>
2023-07-26 12:16:07 +0300

    Update example_1_list.py

--- example_1_list.py
+++ example_1_list.py
@@ -1,8 +1,14 @@
-names =[]
+# input information from keyboar and save it to list
 
+names =[]     
+# ask to enter 3 names in loop
 for _ in range(3):
     name=input("What is your name? ")
     names.append(name)
 
+# output sortet information in terminal
+
 for name in sorted(names):
     print(f"hello, {name}")
+
+# for more information about sorted list read using Python documentation https://docs.python.org/3/howto/sorting.html 
